b5e833baed383e3d50c8985979d5b7e266db53eb
Oriol Angelet <uriaeri@gmail.com>
2024-07-05 17:01:00 +0200

    Initial commit

---
+++ GlowAndShine/GlowAndShine/GlowAndShineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlowAndShineApp.swift
+//  GlowAndShine
+//
+//  Created by Uri on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct GlowAndShineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
